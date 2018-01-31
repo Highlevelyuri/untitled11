fba652dc1cec66230c7a755ffefddca8f362f710
Highlevelyuri <kriucov61944zce3.com@gmail.com>
2018-01-31 12:44:42 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int number= 123;
+	if ( Math.pow (number , 2) == 14)
+        System.out.println( "Квадрат этого числа равен сумме кубов его цифр.");
+	else
+        System.out.println("квадрат этого числа не равен сумме кубов его цифр.");
+    }
+}
